a8ca66dde9628b0ab90d81833c502efbb502d828
YourLocalHomeInvader <109906500+YourLocalHomeInvader@users.noreply.github.com>
2022-07-24 09:15:18 -0400

    Add files via upload

---
+++ Points.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine; 
+using System.Collections;
+namespace Points
+{
+    public class Points
+    {
+        public static void Main()
+        { 
+            var Points = 0f; //Starting Amount
+            var Reset = 1f; //define stats
+            ModAPI.OnDeath += (sender, life) => { //Start on Death
+                Points += 25; //Amount of Points
+                ModAPI.Notify("<color=green>" + "+25 Points. " + "\n <color=orange>" + "Score: " + "<color=yellow>" + Mathf.Round(Points * 10) / 10); //Message that will appear
+            };
+
+            ModAPI.OnItemSpawned += (sender, args) => { //Start on Item Spawned
+                Points += 1; //Amount of Points
+                ModAPI.Notify("<color=green>" + "+1 Point. " + "\n <color=orange>" + "Score: " + "<color=yellow>" + Mathf.Round(Points * 10) / 10); //Message that will appear
+		};
+        }
+    }
+}
